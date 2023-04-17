79550c90d07331dcb7d7756a14da4da01679e1cb
plotiwitolp <plotiwitolp@gmail.com>
2023-04-17 18:11:31 +0400

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,6 @@
   $(document).ready(function () {
     // jQuery код
     $('.toc ul a').on('click', function () {
-      console.log($(this));
       $('.toc ul a').removeClass('active');
       $(this).addClass('active');
     });
