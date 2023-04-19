7535be15aebba57544b649796c2cd55c536a8a4f
plotiwitolp <plotiwitolp@gmail.com>
2023-04-19 15:01:17 +0400

    update

--- portfolio.php
+++ portfolio.php
@@ -3,12 +3,23 @@
 <!-- end header -->
 
 <!-- start меню по содержанию -->
-<?php require_once './templates/toc.php' ?>
+<aside class="toc">
+    <nav>
+        <ul>
+            <li><a href="#test1" class="active">Верстка</a></li>
+            <li><a href="#test8">Посадка на CMS WordPress</a></li>
+            <li><a href="#test8">Калькуляторы</a></li>
+            <li><a href="#test2">Слайдеры</a></li>
+        </ul>
+    </nav>
+</aside>
 <!-- end меню по содержанию -->
 
 <!-- start main -->
 <div class="main">
     <h1>Портфолио</h1>
+    .
+
 </div>
 <!-- end main -->
 
