19827805b9172f9cd5faeb57bafecff70121fa51
plotiwitolp <plotiwitolp@gmail.com>
2023-04-19 15:01:38 +0400

    update

--- fordev.php
+++ fordev.php
@@ -14,7 +14,20 @@
 </div>
 
 <!-- start меню по содержанию -->
-<?php require_once './templates/toc.php' ?>
+<aside class="toc">
+    <nav>
+        <ul>
+            <li><a href="#test1" class="active">О себе</a></li>
+            <li><a href="#test2">Портфолио</a></li>
+            <li><a href="#test8">Технологии</a></li>
+            <li><a href="#test7">Отзывы</a></li>
+            <li><a href="#test6">Преимущества</a></li>
+            <li><a href="#test4">Рассчитать стоимость сайта</a></li>
+            <li><a href="#test3">Подать заявку</a></li>
+            <li><a href="#test10">Контакты</a></li>
+        </ul>
+    </nav>
+</aside>
 <!-- end меню по содержанию -->
 
 <!-- start main -->
--- index.php
+++ index.php
@@ -3,41 +3,40 @@
 <!-- end header -->
 
 <!-- start меню по содержанию -->
-<?php require_once './templates/toc.php' ?>
+<aside class="toc">
+    <nav>
+        <ul>
+            <li><a href="#test1" class="active">О себе</a></li>
+            <li><a href="#test2">Портфолио</a></li>
+            <li><a href="#test8">Технологии</a></li>
+            <li><a href="#test7">Отзывы</a></li>
+            <li><a href="#test6">Преимущества</a></li>
+            <li><a href="#test4">Рассчитать стоимость сайта</a></li>
+            <li><a href="#test3">Подать заявку</a></li>
+            <li><a href="#test10">Контакты</a></li>
+        </ul>
+    </nav>
+</aside>
 <!-- end меню по содержанию -->
 
 <!-- start главный контент -->
 <div class="main">
     <section id="test1">
         <div class="section">
-            <h1>Верхний блок</h1>
+            <h1>О себе</h1>
 
 
         </div>
     </section>
     <section id="test2">
         <div class="section">
-            <h2>Мое портфолио</h2>
-        </div>
-    </section>
-    <section id="test3">
-        <div class="section">
-            <h2>
-                Подать заявку
-            </h2>
+            <h2>Портфолио</h2>
         </div>
     </section>
     <section id="test4">
         <div class="section">
             <h2>
                 Расчет стоимости сайта
-            </h2>
-        </div>
-    </section>
-    <section id="test5">
-        <div class="section">
-            <h2>
-                5 шагов
             </h2>
         </div>
     </section>
@@ -48,13 +47,6 @@
             </h2>
         </div>
     </section>
-    <section id="test7">
-        <div class="section">
-            <h2>
-                Отзывы
-            </h2>
-        </div>
-    </section>
     <section id="test8">
         <div class="section">
             <h2>
@@ -62,10 +54,18 @@
             </h2>
         </div>
     </section>
-    <section id="test9">
+    <section id="test7">
         <div class="section">
             <h2>
-                Часто Задаваемые Вопросы
+                Отзывы
+            </h2>
+        </div>
+    </section>
+
+    <section id="test3">
+        <div class="section">
+            <h2>
+                Подать заявку
             </h2>
         </div>
     </section>
